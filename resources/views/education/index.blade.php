5b08d00b0ac265d88c3abf7e88e59d0f0d1da69e
Richard Omoka <richard.omoka@gmail.com>
2020-05-03 00:36:34 +0300

    -Fixed spelling mistake

--- resources/views/education/index.blade.php
+++ resources/views/education/index.blade.php
@@ -6,11 +6,11 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Education Institution TPS
-                <button type="button" class="btn btn-outline-dark float-right">Add Eduction Record</button>
+                <button type="button" class="btn btn-outline-dark float-right">Add Education Record</button>
                 </div>
 
                 <div class="card-body">
-                  
+
                 </div>
             </div>
         </div>
