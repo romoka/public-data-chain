d9d20bdb20a9734ef959be599c46552a37e32db8
Richard Omoka <richard.omoka@gmail.com>
2020-05-03 23:55:15 +0300

    Updated scripts file

--- hyperledger-composer/script.js
+++ hyperledger-composer/script.js
@@ -3,10 +3,32 @@
  * @param {net.pdc.CreatePerson} person The new person asset
  * @transaction
  */
-async function createPerson(aPerson){
+async function createPerson(tx){
   const factory = getFactory();
-  var newPerson = factory.newResource('net.pdc', 'Person', aPerson.idNumber);
-  const personAssetRegestry = await getAssetRegestry('net.pdc.Person');
-  await personAssetRegestry.add(aPerson);
+  const NAMESPACE = 'net.pdc';
+  var newPerson = factory.newResource(NAMESPACE, 'Person', tx.newPerson.idNumber);
+  newPerson.name = tx.newPerson.name;
+  newPerson.sex = tx.newPerson.sex;
+  newPerson.dob = tx.newPerson.dob;
+  newPerson.passportNo = tx.newPerson.passportNo;
+  newPerson.photo = tx.newPerson.photo;
+  newPerson.idScan = tx.newPerson.idScan;
+  newPerson.address = tx.newPerson.address;
+  
+  
+  var birthCertificateConcept = factory.newConcept(NAMESPACE, 'Birth');
+     birthCertificateConcept.certificateNo = tx.newPerson.birthCertificate.certificateNo;
+    birthCertificateConcept.placeOfBirth = tx.newPerson.birthCertificate.placeOfBirth;
+   birthCertificateConcept. hospitalOfBirth = tx.newPerson.birthCertificate.hospitalOfBirth;
+    birthCertificateConcept.fatherIdNumber = tx.newPerson.birthCertificate.fatherIdNumber;
+    birthCertificateConcept.motherIdNumber = tx.newPerson.birthCertificate.motherIdNumber;
+    birthCertificateConcept.informantIdNumber = tx.newPerson.birthCertificate.informantIdNumber;
+    birthCertificateConcept.registrar = tx.newPerson.birthCertificate.registrar;
+    birthCertificateConcept.dob = tx.newPerson.birthCertificate.dob;
+    birthCertificateConcept.doi = tx.newPerson.birthCertificate.doi; 
+   newPerson.birthCertificate = birthCertificateConcept;
+  
+  const personAssetRegestry = await getAssetRegistry('net.pdc.Person');
+  await personAssetRegestry.add(newPerson);
 }
 
