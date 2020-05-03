c9f79ae6d6d517fa49a97c146ca71c1a8a5cc753
Richard Omoka <richard.omoka@gmail.com>
2020-05-03 20:07:06 +0300

    -Saving hyperledger composer files

--- app/Education.php
+++ app/Education.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Education extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'pid_number', 'level', 'institution','start_date','end_date','grade'
+    ];
+
 }
